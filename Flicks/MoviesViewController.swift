7f2e9d51aa7ebe23730f318f115149185112706c
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-01-17 15:57:14 -0800

    Add network request snippet to 'viewDidLoad' function. - Add API key for themoviedb.org - Add constant for URL - Parse JSON into an NSDictionary into constant 'responseDictionary'

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -17,6 +17,27 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.dataSource = self
         tableView.delegate = self
         
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            
+                    }
+                }
+        });
+        task.resume()
+        
         // Do any additional setup after loading the view.
     }
 
