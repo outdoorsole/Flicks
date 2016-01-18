0403eca124e5b157dd66fe0e334659bc5b4b39f5
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-01-17 18:21:27 -0800

    Add outlets for 'title' and 'overview' labels (and custom class of MovieCell) to Main.storyboard

--- Flicks/MovieCell.swift
+++ Flicks/MovieCell.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class MovieCell: UITableViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
