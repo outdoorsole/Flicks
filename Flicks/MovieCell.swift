c09eb56c64c14b610784eadbe9145b2e1526d1bd
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-01-17 18:13:30 -0800

    Add 2 custom labels ('title' and 'overview') and custom cell class (MovieCell).

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Mari Gordon on 1/17/16.
+//  Copyright © 2016 Maribel Montejano. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
