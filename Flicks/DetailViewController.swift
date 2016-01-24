d08eae04f71b203a391e9afa7f35df9c59bbf851
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-01-24 10:35:07 -0800

    Scroll feature is working: - Add an outlet for the infoView (the 'title' & 'overview' info) - Set the content size for the scroll view, referencing the infoView frame width & height

--- Flicks/DetailViewController.swift
+++ Flicks/DetailViewController.swift
@@ -14,12 +14,16 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var infoView: UIView!
     
     var movie: NSDictionary!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Set the content size
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.origin.y + infoView.frame.size.height)
+        
         let title = movie["title"] as? String
         titleLabel.text = title
         
