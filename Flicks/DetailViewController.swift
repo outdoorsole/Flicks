c7e9394038d5975626fbd19c9e350a85a4bfa333
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-01-24 08:56:44 -0800

    Uncomment method 'prepareForSegue' (to run right before segue).

--- Flicks/DetailViewController.swift
+++ Flicks/DetailViewController.swift
@@ -28,7 +28,6 @@ class DetailViewController: UIViewController {
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -36,6 +35,4 @@ class DetailViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
-
 }
