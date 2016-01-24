03bc33e5afd5f3c3f470d7ff31d23565130eb787
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-01-24 09:15:05 -0800

    Modify 'prepareForSegue' function to reference cell for the selected movie - Reference the index path to get the cell that was tapped on (casted as a UITableViewCell) - Use it to index into the array to get the right movie and set the segue - Add print statement to 'viewDidLoad' in DetailsViewController to check if data was passed

--- Flicks/DetailViewController.swift
+++ Flicks/DetailViewController.swift
@@ -19,6 +19,8 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print(movie)
+        
         // Do any additional setup after loading the view.
     }
 
@@ -27,12 +29,15 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
+    */
 }
--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -115,14 +115,24 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
     }
     
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPathForCell(cell)
+        let movie = movies![indexPath!.row]
+        
+        let detailViewController = segue.destinationViewController as! DetailViewController
+        detailViewController.movie = movie
+        
+        print("prepare for segue called")
+        
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
